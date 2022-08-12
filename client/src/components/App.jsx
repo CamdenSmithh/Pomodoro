9da2a96acfae966055add619774302b33ae938ed
Camden Smith <camdensmithh@gmail.com>
2022-08-12 08:27:44 -0700

    Adding Settings state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Timer from './Timer.jsx';
 
 const styles = {
   container: {
+    fontFamily: 'futura',
   },
   h1: {
+    borderBottom: 'solid',
+    fontSize: '50px',
+    marginTop: '10px',
   },
 };
 
 export default function App() {
+  const [settings, setSettings] = (false);
+
   return (
     <div style={styles.container}>
       <h1 style={styles.h1}>Pomodoro App</h1>
-      <Timer />
+      <Timer settings={settings} />
     </div>
   );
 }
