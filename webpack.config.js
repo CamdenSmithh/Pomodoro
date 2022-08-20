03f03900e51856fd9bdd87bbecacef9b57a065a5
Camden Smith <camdensmithh@gmail.com>
2022-08-19 18:52:04 -0700

    Adjusting directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
+const DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
